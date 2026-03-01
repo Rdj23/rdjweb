3899759ae7ab54e400178715e8c3e7876c62d389
Rohan <rohan.jadhav@clevertap.com>
2026-03-02 01:11:39 +0530

    MSG-dndEmail

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ export default function App() {
       Site: {
         ...updatedProfile,
         "MSG-email": true,
-        "MSG-dnd": false,
+        "MSG-dndEmail": false,
       },
     });
     
